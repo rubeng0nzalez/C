0abe795e500875eecc4446fee8593f73b58741e9
Kalkini <rgonzalez.mywork@gmail.com>
2023-03-17 01:25:44 -0400

    initial commit

---
+++ HelloWorldFromEclipse/src/HelloWorldFromEclipse.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : HelloWorldFromEclipse.c
+ Author      : Ruben
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!!Hello World From Eclipse!!!"); /* prints !!!Hello World From Eclipse!!! */
+	return EXIT_SUCCESS;
+}
---
+++ PrintMessage/main.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+
+int main(){
+
+    printf("Message from a C program\nNothing complicated...");
+
+    return 0;
+}
